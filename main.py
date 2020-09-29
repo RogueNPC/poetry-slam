117850a874d075a9cce462f7fd0961d83299b1c5
RogueNPC <jordanbtansay@gmail.com>
2020-09-29 14:02:29 -0700

    lines_printed_random function created

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 
 def get_file_lines(filename):
     with open(filename, "r") as outfile:
@@ -7,9 +8,17 @@ def get_file_lines(filename):
 poem = get_file_lines("cs1.0/poetry-slam/poem.txt")
 
 def lines_printed_backwards(lines_list):
-    line_num = len(lines_list)
+    lineNum = len(lines_list)
     for line in reversed(lines_list):
-        print(line_num, line, end=' ')
-        line_num -= 1
+        print(lineNum, line, end=' ')
+        lineNum -= 1
 
-lines_printed_backwards(poem)
+#lines_printed_backwards(poem)
+
+def lines_printed_random(lines_list):
+    lastIndex = len(lines_list)-1
+    for line in lines_list:
+        randomIndex = randint(1, lastIndex)
+        print(lines_list[randomIndex], end=' ')
+
+lines_printed_random(poem)
