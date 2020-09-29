d8417ff21df21c82e1679f7630307ea2db20821d
RogueNPC <jordanbtansay@gmail.com>
2020-09-29 13:44:16 -0700

    lines_printed_backwards function created

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ def get_file_lines(filename):
         lines = outfile.readlines()
     return lines
 
-get_file_lines('poem.txt')
+poem = get_file_lines("cs1.0/poetry-slam/poem.txt")
+
+def lines_printed_backwards(lines_list):
+    line_num = len(lines_list)
+    for line in reversed(lines_list):
+        print(line_num, line, end=' ')
+        line_num -= 1
+
+lines_printed_backwards(poem)
